a3b8b6b9b12f69c1f02e8c3ef45d1b23fcd980af
Rabbi Islam Rony <ronya4927@gmail.com>
2023-10-21 15:12:53 +0600

    overview cheeck

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
  mode: 'development',
@@ -54,5 +54,6 @@ module.exports = {
         filename:'index.html',
         template:'src/template.html',
     }),
+    new BundleAnalyzerPlugin(),
  ],
 }
